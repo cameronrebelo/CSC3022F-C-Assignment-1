b62fa2013ce80244d0e176034f1c0f3b4b8eba98
CamDan379 <89079757+CamDan379@users.noreply.github.com>
2022-02-27 21:36:36 +0200

    Created main file and header as well as Makefile

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <main.h>
+#include <string>
+#include <iostream>
+
+struct TagStruct{
+    std::string name;
+    int pairs;
+    std::string text;
+};
+
+int RBLCAM001::main(std::string argc, char * argv[]){
+    while(true){
+        
+    }
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN
+#define _MAIN
+
+namespace RBLCAM001 {
+    int main(int argc, char **argv) {};
+}
+
+#endif
